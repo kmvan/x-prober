1c5939095e91a4ab91643551cb881cee86b74539
kmvan <kmvan.com@gmail.com>
2018-10-02 16:16:08 +0800

    update version 1.7.2

--- src/Config/Api.php
+++ src/Config/Api.php
@@ -4,7 +4,7 @@ namespace InnStudio\Prober\Config;
 
 class Api
 {
-    public static $APP_VERSION    = '1.7.1';
+    public static $APP_VERSION    = '1.7.2';
     public static $APP_NAME       = 'X Prober';
     public static $APP_URL        = 'https://github.com/kmvan/x-prober';
     public static $AUTHOR_URL     = 'https://inn-studio.com/prober';
