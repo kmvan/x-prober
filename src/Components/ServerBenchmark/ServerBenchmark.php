ec49c1d1ec369986c94cd817f95e3d5148acdab1
kmvan <kmvan.com@gmail.com>
2019-02-09 19:09:55 +0800

    add vultr PHP7.3 marks

--- src/Components/ServerBenchmark/ServerBenchmark.php
+++ src/Components/ServerBenchmark/ServerBenchmark.php
@@ -56,47 +56,52 @@ HTML;
     {
         $items = array(
             array(
-                'label'   => I18nApi::_('Amazon/EC2'),
+                'label'   => 'Vultr/PHP7.3',
+                'url'     => 'https://www.vultr.com/?ref=7826363-4F',
+                'content' => 3305,
+            ),
+            array(
+                'label'   => 'Amazon/EC2/PHP7.2',
                 'url'     => 'https://aws.amazon.com/',
                 'content' => 3150,
             ),
             array(
-                'label'   => I18nApi::_('VPSSERVER/KVM'),
+                'label'   => 'VPSSERVER/KVM/PHP7.2',
                 'url'     => 'https://www.vpsserver.com/?affcode=32d56f2dd1b6',
                 'content' => 3125,
             ),
             array(
-                'label'   => I18nApi::_('SpartanHost/KVM'),
+                'label'   => 'SpartanHost/KVM/PHP7.2',
                 'url'     => 'https://billing.spartanhost.net/aff.php?aff=801',
                 'content' => 3174,
             ),
             array(
-                'label'   => I18nApi::_('Aliyun/ECS'),
+                'label'   => 'Aliyun/ECS/PHP7.2',
                 'url'     => 'https://promotion.aliyun.com/ntms/act/ambassador/sharetouser.html?userCode=0nry1oii&amp;utm_source=0nry1oii',
                 'content' => 3302,
             ),
             array(
-                'label'   => I18nApi::_('Vultr'),
+                'label'   => 'Vultr/PHP7.2',
                 'url'     => 'https://www.vultr.com/?ref=7256513',
                 'content' => 3182,
             ),
             array(
-                'label'   => I18nApi::_('RamNode'),
+                'label'   => 'RamNode/PHP7.2',
                 'url'     => 'https://clientarea.ramnode.com/aff.php?aff=4143',
                 'content' => 3131,
             ),
             array(
-                'label'   => I18nApi::_('Linode'),
+                'label'   => 'Linode/PHP7.2',
                 'url'     => 'https://www.linode.com/?r=2edf930598b4165760c1da9e77b995bac72f8ad1',
                 'content' => 3091,
             ),
             array(
-                'label'   => I18nApi::_('Tencent'),
+                'label'   => 'Tencent/PHP7.2',
                 'url'     => 'https://cloud.tencent.com/',
                 'content' => 3055,
             ),
             array(
-                'label'   => I18nApi::_('BandwagonHOST/SSD'),
+                'label'   => 'BandwagonHOST/KVM/PHP7.2',
                 'url'     => 'https://bandwagonhost.com/aff.php?aff=34116',
                 'content' => 2181,
             ),
