08cc79bfbd4e53b4c511580719f998890a714b1c
kleveralt <klever1989@gmail.com>
2019-03-04 14:57:46 +0800

    Update HelperApi.php
    disable front end or cdn caching

--- src/Components/Helper/HelperApi.php
+++ src/Components/Helper/HelperApi.php
@@ -122,6 +122,11 @@ HTML;
     public static function dieJson($data)
     {
         \header('Content-Type: application/json');
+        \header("Expires: 0");
+        \header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        \header("Cache-Control: no-store, no-cache, must-revalidate");
+        \header("Pragma: no-cache");
+
 
         die(\json_encode($data));
     }
