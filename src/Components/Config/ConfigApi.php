d08cbc921dcaa55a980cbf889a6fc93e35e16366
kmvan <kmvan.com@gmail.com>
2019-05-14 23:31:43 +0800

    update to 2.4.0

--- src/Components/Config/ConfigApi.php
+++ src/Components/Config/ConfigApi.php
@@ -7,13 +7,13 @@ namespace InnStudio\Prober\Components\Config;
 
 class ConfigApi
 {
-    public static $APP_VERSION                 = '2.3.2';
+    public static $APP_VERSION                 = '2.4.0';
     public static $APP_NAME                    = 'X Prober';
     public static $APP_URL                     = 'https://github.com/kmvan/x-prober';
     public static $AUTHOR_URL                  = 'https://inn-studio.com/prober';
     public static $UPDATE_PHP_URLS             = array('https://raw.githubusercontent.com/kmvan/x-prober/master/dist/prober.php', 'https://api.inn-studio.com/download/?id=xprober');
     public static $AUTHOR_NAME                 = 'INN STUDIO';
     public static $CHANGELOG_URL               = 'https://raw.githubusercontent.com/kmvan/x-prober/master/CHANGELOG.md';
-    public static $LATEST_PHP_STABLE_VERSION   = '7.3.4';
-    public static $LATEST_NGINX_STABLE_VERSION = '1.14.2';
+    public static $LATEST_PHP_STABLE_VERSION   = '7.3.5';
+    public static $LATEST_NGINX_STABLE_VERSION = '1.16.0';
 }
