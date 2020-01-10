196d5cd5d6de12fd4b3d7aa44a730bebbf0030a4
Km.Van <kmvan.com@gmail.com>
2020-01-11 00:17:02 +0800

    Update raspberrypi.js

--- temp_modules/raspberrypi.js
+++ temp_modules/raspberrypi.js
@@ -1,32 +1,35 @@
+/**
+ * Raspberry PI Series (Linux) Temperature CPU
+ *
+ * @usage `node ./raspberrypi.js`
+ */
+
 //import sf and http modules
-var fs=require("fs");
-var http = require("http");
-var temp;
+const fs = require('fs')
+const http = require('http')
+let temp
 
 //read CPU temperature every second
-setInterval(function() {temp = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");},1000);
+setInterval(() => {
+  temp = fs.readFileSync('/sys/class/thermal/thermal_zone0/temp')
+}, 1000)
 
 //creat http server
-var server = http.createServer(function (request, response) {
-//write response header
-response.writeHead(200, {
-//defining document type then charset "utf-8"
-'Content-Type': 'application/json;charset="utf-8"',
-//CORS
-'Access-Control-Allow-Origin': "*"
-});
-
-var info = {
-a : [{
-id : "cpu",
-name : "CPU 温度",
-celsius : temp/1000,
-}]
-};
-
-//transform object into string, and output with .end() method
-response.end(JSON.stringify(info.a));
-});
-
+const server = http.createServer((request, response) => {
+  //write response header
+  response.writeHead(200, {
+    //defining document type then charset "utf-8"
+    'Content-Type': 'application/json;charset="utf-8"',
+    //CORS
+    'Access-Control-Allow-Origin': "*"
+  })
+  const items = [{
+    id: 'cpu',
+    name: 'CPU Temperature',
+    celsius: temp / 1000,
+  }]
+  //transform object into string, and output with .end() method
+  response.end(JSON.stringify(items))
+})
 //listen port and IP address
-server.listen(4096,'127.0.0.1');
+server.listen(4096, '127.0.0.1')
