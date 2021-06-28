f152885e5d0c453ef9cca2c80befdf992e65b33f
Km.Van <kmvan.com@gmail.com>
2021-06-28 23:13:44 +0800

    remove ResizeObserver typings

--- src/Components/Utils/src/components/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface ResizeObserver {
-  observe(target: Element): void
-  unobserve(target: Element): void
-  disconnect(): void
-}
-declare var ResizeObserver: {
-  prototype: ResizeObserver
-  new (callback: ResizeObserverCallback): ResizeObserver
-}
