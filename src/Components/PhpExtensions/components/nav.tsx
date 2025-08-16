6bc2cab53c071ee218bca19dc7f40cc03525a4a0
Km.Van <kmvan.com@gmail.com>
2025-08-16 14:31:51 +0800

    adjust language

--- src/Components/PhpExtensions/components/nav.tsx
+++ src/Components/PhpExtensions/components/nav.tsx
@@ -3,10 +3,11 @@ import type { FC } from 'react';
 import { gettext } from '@/Components/Language/index.ts';
 import { NavItem } from '@/Components/Nav/components/item.tsx';
 import { PhpExtensionsConstants } from './constants.ts';
-import { PhpExtensionsStore } from './store.ts';export const PhpExtensionsNav: FC = observer(() => {
+import { PhpExtensionsStore } from './store.ts';
+export const PhpExtensionsNav: FC = observer(() => {
   const { pollData } = PhpExtensionsStore;
   if (!pollData) {
     return null;
   }
-  return <NavItem id={PhpExtensionsConstants.id} title={gettext('Ext')} />;
+  return <NavItem id={PhpExtensionsConstants.id} title={gettext('PHP Ext')} />;
 });
--- src/Components/PhpInfo/components/nav.tsx
+++ src/Components/PhpInfo/components/nav.tsx
@@ -3,10 +3,11 @@ import type { FC } from 'react';
 import { gettext } from '@/Components/Language/index.ts';
 import { NavItem } from '@/Components/Nav/components/item.tsx';
 import { PhpInfoConstants } from './constants.ts';
-import { PhpInfoStore } from './store.ts';export const PhpInfoNav: FC = observer(() => {
+import { PhpInfoStore } from './store.ts';
+export const PhpInfoNav: FC = observer(() => {
   const { pollData } = PhpInfoStore;
   if (!pollData) {
     return null;
   }
-  return <NavItem id={PhpInfoConstants.id} title={gettext('Ext')} />;
+  return <NavItem id={PhpInfoConstants.id} title={gettext('PHP Info')} />;
 });
